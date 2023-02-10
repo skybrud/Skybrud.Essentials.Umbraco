ffcef7f9fcab8adfa7250cfc199282428e15a331
Anders Bjerner <abjerner@skybrud.dk>
2023-02-10 18:51:56 +0100

    Added "GetBoolean..." extension methods to the "PublishedElementExtensions" class

--- src/Skybrud.Essentials.Umbraco/PublishedElementExtensions.cs
+++ src/Skybrud.Essentials.Umbraco/PublishedElementExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using Skybrud.Essentials.Strings;
 using Skybrud.Essentials.Strings.Extensions;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Extensions;
@@ -10,6 +11,75 @@ namespace Skybrud.Essentials.Umbraco {
     /// Static class with various extension methods for <see cref="IPublishedElement"/>.
     /// </summary>
     public static class PublishedElementExtensions {
+
+        #region Boolean
+
+        /// <summary>
+        /// Returns the boolean value of the property with the specified <paramref name="alias"/>, or <see langwird="false"/> if a matching property could not be found or it's value converted to a <see cref="bool"/> instance.
+        /// </summary>
+        /// <param name="element">The element holding the property.</param>
+        /// <param name="alias">The alias of the property.</param>
+        /// <returns>An instance of <see cref="bool"/>.</returns>
+        public static bool GetBoolean(this IPublishedElement element, string alias) {
+            return element.Value(alias) switch {
+                bool boolean => boolean,
+                int number => number == 1,
+                string str => StringUtils.ParseBoolean(str),
+                _ => false
+            };
+        }
+
+        /// <summary>
+        /// Returns the boolean value of the property with the specified <paramref name="alias"/>, or <see langword="null"/> if a matching property could not be found or it's value converted to a <see cref="bool"/> instance.
+        /// </summary>
+        /// <param name="element">The element holding the property.</param>
+        /// <param name="alias">The alias of the property.</param>
+        /// <returns>An instance of <see cref="bool"/> if successful; otherwise, <see langword="null"/>.</returns>
+        public static bool? GetBooleanOrNull(this IPublishedElement? element, string alias) {
+            return element?.Value(alias) switch {
+                bool boolean => boolean,
+                int number => number == 1,
+                string str => StringUtils.ParseBooleanOrNull(str),
+                _ => null
+            };
+        }
+
+        /// <summary>
+        /// Attempts to get a boolean value from the property with the specified <paramref name="alias"/>.
+        /// </summary>
+        /// <param name="element">The element holding the property.</param>
+        /// <param name="alias">The alias of the property.</param>
+        /// <param name="result">When this method returns, holds the <see cref="bool"/> value if successful; otherwise, <see langword="false"/>.</param>
+        /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+        public static bool TryGetBoolean(this IPublishedElement? element, string alias, out bool result) {
+            if (element.TryGetBoolean(alias, out bool? guid)) {
+                result = guid.Value;
+                return true;
+            }
+            result = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Attempts to get a boolean value from the property with the specified <paramref name="alias"/>.
+        /// </summary>
+        /// <param name="element">The element holding the property.</param>
+        /// <param name="alias">The alias of the property.</param>
+        /// <param name="result">When this method returns, holds the <see cref="bool"/> value if successful; otherwise, <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if successful; otherwise, <see langword="false"/>.</returns>
+        public static bool TryGetBoolean(this IPublishedElement? element, string alias, [NotNullWhen(true)] out bool? result) {
+
+            if (element is not null && element.HasProperty(alias)) {
+                result = element.GetBooleanOrNull(alias);
+                return result is not null;
+            }
+
+            result = null;
+            return false;
+
+        }
+
+        #endregion
 
         #region Guid
 
