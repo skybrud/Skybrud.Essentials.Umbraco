814d5d386fbf88609c24a7889f768074ebfb2595
Anders Bjerner <abj@limbo.works>
2025-06-10 14:11:41 +0200

    Added constants for Umbraco's media types
    Umbraco already has the "Constants.Conventions.MediaTypes" nested class with similar constants, but the constants are a bit messy following different namings, and the "Constants" class may also conflict with a constants "Constants"  namespace in individual solutions.

---
+++ src/Skybrud.Essentials.Umbraco/Media/UmbracoMediaTypes.cs
@@ -0,0 +1,38 @@
+﻿namespace Skybrud.Essentials.Umbraco.Media;
+
+/// <summary>
+/// Static class with constants for Umbraco media types.
+/// </summary>
+public static class UmbracoMediaTypes {
+
+    /// <summary>
+    /// Gets the alias of the <strong>Article</strong> media type.
+    /// </summary>
+    public const string Article = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.ArticleAlias;
+
+    /// <summary>
+    /// Gets the alias of the <strong>File</strong> media type.
+    /// </summary>
+    public const string File = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.File;
+
+    /// <summary>
+    /// Gets the alias of the <strong>Folder</strong> media type.
+    /// </summary>
+    public const string Folder = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.Folder;
+
+    /// <summary>
+    /// Gets the alias of the <strong>Image</strong> media type.
+    /// </summary>
+    public const string Image = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.Image;
+
+    /// <summary>
+    /// Gets the alias of the <strong>Vector Graphics</strong> media type.
+    /// </summary>
+    public const string VectorGraphics = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.VectorGraphicsAlias;
+
+    /// <summary>
+    /// Gets the alias of the <strong>>Video</strong> media type.
+    /// </summary>
+    public const string Video = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.VideoAlias;
+
+}
