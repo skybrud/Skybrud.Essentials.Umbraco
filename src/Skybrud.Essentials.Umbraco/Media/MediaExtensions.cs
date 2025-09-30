3ef88564d78d663865a85aabc0add3b2f43a932f
Anders Bjerner <abj@limbo.works>
2025-09-30 11:16:58 +0200

    Misc work on the "CreateMedia" and "CreateFolder" extension methods
    Umbraco is quite picky when creating a new media without a parent. For instance, the a "MediaService.CreateMedia" method overload takes "IMedia" as the parent, but the method will throw an exception if the specified parent is null - even though the "parent" parameter is marked as nullable. Another overload takes a GUID as the parent key, but the parameter is not nullable, and specifying Guid.Empty also triggers an exception.
    
    - fixed misc issues with speciyfing a nullable parent
    - added misc new method overloads
    - etc.

--- src/Skybrud.Essentials.Umbraco/Media/MediaExtensions.cs
+++ src/Skybrud.Essentials.Umbraco/Media/MediaExtensions.cs
@@ -102,6 +102,32 @@ public static class MediaExtensions {
     }
 
     /// <summary>
+    /// Creates a new media item with the specified <paramref name="name"/>, <paramref name="parentId"/>, and <paramref name="mediaTypeAlias"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parentId">The numeric ID of the parent media, if any.</param>
+    /// <param name="mediaTypeAlias">The alias of the media type.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created media.</returns>
+    public static IMedia CreateMedia(this IMediaService mediaService, string name, int? parentId, string mediaTypeAlias, int userId = -1) {
+        return mediaService.CreateMedia(name, parentId ?? -1, mediaTypeAlias, userId);
+    }
+
+    /// <summary>
+    /// Creates a new media item with the specified <paramref name="name"/>, <paramref name="parentKey"/>, and <paramref name="mediaTypeAlias"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parentKey">The GUID key of the parent media, if any.</param>
+    /// <param name="mediaTypeAlias">The alias of the media type.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created media.</returns>
+    public static IMedia CreateMedia(this IMediaService mediaService, string name, Guid? parentKey, string mediaTypeAlias, int userId = -1) {
+        return parentKey is null ? mediaService.CreateMedia(name, -1, mediaTypeAlias, userId) : mediaService.CreateMedia(name, parentKey.Value, mediaTypeAlias, userId);
+    }
+
+    /// <summary>
     /// Creates a new media item with the specified <paramref name="key"/>, <paramref name="name"/>, <paramref name="parentId"/>, and <paramref name="mediaTypeAlias"/>.
     /// </summary>
     /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
@@ -113,6 +139,20 @@ public static class MediaExtensions {
     /// <returns>An instance of <see cref="IMedia"/> representing the created media.</returns>
     public static IMedia CreateMedia(this IMediaService mediaService, Guid key, string name, int parentId, string mediaTypeAlias, int userId = -1) {
         return mediaService.CreateMedia(name, parentId, mediaTypeAlias, userId).SetKey(key);
+    }
+
+    /// <summary>
+    /// Creates a new media item with the specified <paramref name="key"/>, <paramref name="name"/>, <paramref name="parentId"/>, and <paramref name="mediaTypeAlias"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="key">The key of the media item.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parentId">The numeric ID of the parent media.</param>
+    /// <param name="mediaTypeAlias">The alias of the media type.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created media.</returns>
+    public static IMedia CreateMedia(this IMediaService mediaService, Guid key, string name, int? parentId, string mediaTypeAlias, int userId = -1) {
+        return CreateMedia(mediaService, key, name, parentId ?? -1, mediaTypeAlias, userId);
     }
 
     /// <summary>
@@ -130,6 +170,20 @@ public static class MediaExtensions {
     }
 
     /// <summary>
+    /// Creates a new media item with the specified <paramref name="key"/>, <paramref name="name"/>, <paramref name="parentKey"/>, and <paramref name="mediaTypeAlias"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="key">The key of the media item.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parentKey">The GUID key of the parent media.</param>
+    /// <param name="mediaTypeAlias">The alias of the media type.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created media.</returns>
+    public static IMedia CreateMedia(this IMediaService mediaService, Guid key, string name, Guid? parentKey, string mediaTypeAlias, int userId = -1) {
+        return CreateMedia(mediaService, name, parentKey, mediaTypeAlias, userId).SetKey(key);
+    }
+
+    /// <summary>
     /// Creates a new media item with the specified <paramref name="key"/>, <paramref name="name"/>, <paramref name="parent"/>, and <paramref name="mediaTypeAlias"/>.
     /// </summary>
     /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
@@ -140,7 +194,69 @@ public static class MediaExtensions {
     /// <param name="userId">The ID of the user creating the media.</param>
     /// <returns>An instance of <see cref="IMedia"/> representing the created media.</returns>
     public static IMedia CreateMedia(this IMediaService mediaService, Guid key, string name, IMedia? parent, string mediaTypeAlias, int userId = -1) {
-        return mediaService.CreateMedia(name, parent?.Key ?? Guid.Empty, mediaTypeAlias, userId).SetKey(key);
+        return mediaService.CreateMedia(name, parent?.Id ?? -1, mediaTypeAlias, userId).SetKey(key);
+    }
+
+    /// <summary>
+    /// Creates a new folder with the specified <paramref name="name"/> and <paramref name="parentId"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="name">The name of the folder.</param>
+    /// <param name="parentId">The numeric ID of the parent media, if any.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created folder.</returns>
+    public static IMedia CreateFolder(this IMediaService mediaService, string name, int? parentId, int userId = -1) {
+        return mediaService.CreateMedia(name, parentId, Constants.Conventions.MediaTypes.Folder, userId);
+    }
+
+    /// <summary>
+    /// Creates a new folder with the specified <paramref name="name"/> and <paramref name="parentKey"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="name">The name of the folder.</param>
+    /// <param name="parentKey">The GUID key of the parent media, if any.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created folder.</returns>
+    public static IMedia CreateFolder(this IMediaService mediaService, string name, Guid? parentKey, int userId = -1) {
+        return CreateMedia(mediaService, name, parentKey, Constants.Conventions.MediaTypes.Folder, userId);
+    }
+
+    /// <summary>
+    /// Creates a new folder media item with the specified <paramref name="name"/> and <paramref name="parent"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parent">The parent media, if any.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created folder.</returns>
+    public static IMedia CreateFolder(this IMediaService mediaService, string name, IMedia? parent, int userId = -1) {
+        return mediaService.CreateMedia(name, parent?.Id ?? -1, Constants.Conventions.MediaTypes.Folder, userId);
+    }
+
+    /// <summary>
+    /// Creates a new folder media item with the specified <paramref name="key"/>, <paramref name="name"/> and <paramref name="parentId"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="key">The key of the media item.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parentId">The numeric ID of the parent media, if any.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created folder.</returns>
+    public static IMedia CreateFolder(this IMediaService mediaService, Guid key, string name, int? parentId, int userId = -1) {
+        return CreateFolder(mediaService, name, parentId, userId).SetKey(key);
+    }
+
+    /// <summary>
+    /// Creates a new folder media item with the specified <paramref name="key"/>, <paramref name="name"/> and <paramref name="parentKey"/>.
+    /// </summary>
+    /// <param name="mediaService">The current <see cref="IMediaService"/> instance.</param>
+    /// <param name="key">The key of the media item.</param>
+    /// <param name="name">The name of the media item.</param>
+    /// <param name="parentKey">The GUID key of the parent media, if any.</param>
+    /// <param name="userId">The ID of the user creating the media.</param>
+    /// <returns>An instance of <see cref="IMedia"/> representing the created folder.</returns>
+    public static IMedia CreateFolder(this IMediaService mediaService, Guid key, string name, Guid? parentKey, int userId = -1) {
+        return CreateFolder(mediaService, name, parentKey, userId).SetKey(key);
     }
 
     /// <summary>
@@ -153,7 +269,7 @@ public static class MediaExtensions {
     /// <param name="userId">The ID of the user creating the media.</param>
     /// <returns>An instance of <see cref="IMedia"/> representing the created folder.</returns>
     public static IMedia CreateFolder(this IMediaService mediaService, Guid key, string name, IMedia? parent, int userId = -1) {
-        return mediaService.CreateMedia(name, parent?.Key ?? Guid.Empty, Constants.Conventions.MediaTypes.Folder, userId).SetKey(key);
+        return CreateFolder(mediaService, name, parent, userId).SetKey(key);
     }
 
 }
