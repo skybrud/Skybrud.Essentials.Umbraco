8298c8c65f18884e0f20cb52f04e2c66b9e4b107
Anders Bjerner <abj@limbo.works>
2026-02-09 10:02:46 +0100

    Added "GetSearcher" extension method for "IIndex"

---
+++ src/Skybrud.Essentials.Umbraco/Examine/ExamineIndexExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Examine;
+
+namespace Skybrud.Essentials.Umbraco.Examine;
+
+/// <summary>
+/// Static class with extension methods for <see cref="IIndex"/>.
+/// </summary>
+public static class ExamineIndexExtensions {
+
+    /// <summary>
+    /// Same as calling the <see cref="IIndex.Searcher"/> property, but being a method, this looks prettier in a method chain.
+    /// </summary>
+    /// <param name="index">The index.</param>
+    /// <returns>The searcher associated with the index.</returns>
+    public static ISearcher GetSearcher(this IIndex index) {
+        return index.Searcher;
+    }
+
+}
