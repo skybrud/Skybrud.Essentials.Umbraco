447cd143e78b297e9a6d13b816b22089beb57a5b
Anders Bjerner <abj@limbo.works>
2026-08-25 12:46:56 +0200

    Added new "icons" extension

---
+++ src/Skybrud.Essentials.Umbraco/Manifests/Extensions/Icons/IconsExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Skybrud.Essentials.Umbraco.Manifests.Extensions.Icons;
+
+public class IconsExtension : IExtension {
+
+    public string Type => "icons";
+
+    public required string Alias { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Js { get; init; }
+
+}
