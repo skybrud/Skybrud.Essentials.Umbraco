94cad973d587aad841b11bce40433b1d7676ddd7
Anders Bjerner <abj@limbo.works>
2026-09-25 15:40:08 +0200

    Added static "UmbracoMediaTypeAliases" and "UmbracoMediaTypeKeys" classes
    The classes add constants for the aliases and keys respectively for Umbraco's build-in media types
    
    Umbraco already defines constants for the aliases, but one may experience naming collisions since Umbraco has a class simply named "Constants"...

---
+++ src/Skybrud.Essentials.Umbraco/Constants/UmbracoMediaTypeAliases.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Umbraco.Constants;
+
+/// <summary>
+/// Constants for aliases of Umbraco's build-in media types.
+///
+/// Similar to <see cref="global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes"/>, but this class may provide easier access.
+/// </summary>
+public class UmbracoMediaTypeAliases {
+
+    public const string Image = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.Image;
+
+    public const string File = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.File;
+
+    public const string Folder = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.Folder;
+
+    public const string Article = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.ArticleAlias;
+
+    public const string Audio = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.AudioAlias;
+
+    public const string Video = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.VideoAlias;
+
+    public const string VectorGraphics = global::Umbraco.Cms.Core.Constants.Conventions.MediaTypes.VectorGraphicsAlias;
+
+    /// <summary>
+    /// Alias of <see cref="VectorGraphics"/>.
+    /// </summary>
+    public const string Svg = VectorGraphics;
+
+}
---
+++ src/Skybrud.Essentials.Umbraco/Constants/UmbracoMediaTypeKeys.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Skybrud.Essentials.Umbraco.Constants;
+
+/// <summary>
+/// Constants for Umbraco media type keys.
+///
+/// Build-in data types are created during installation, and Umbraco ensures that the media types are created with the
+/// same keys on all installations. This means that you can use these keys to identify media types in your code,
+/// without having to rely on the name of the media type, which can be changed by the user.
+/// </summary>
+public static class UmbracoMediaTypeKeys {
+
+    public static readonly Guid Article = new("a43e3414-9599-4230-a7d3-943a21b20122");
+
+    public static readonly Guid Audio = new("a5ddeee0-8fd8-4cee-a658-6f1fcdb00de3");
+
+    public static readonly Guid File = new("4c52d8ab-54e6-40cd-999c-7a5f24903e4d");
+
+    public static readonly Guid Folder = new("f38bd2d7-65d0-48e6-95dc-87ce06ec2d3d");
+
+    public static readonly Guid Image = new("cc07b313-0843-4aa8-bbda-871c8da728c8");
+
+    public static readonly Guid VectorGraphics = new("c4b1efcf-a9d5-41c4-9621-e9d273b52a9c");
+
+    public static readonly Guid Video = new("f6c515bb-653c-4bdc-821c-987729ebe327");
+
+    /// <summary>
+    /// Alias of <see cref="VectorGraphics"/>.
+    /// </summary>
+    public static readonly Guid Svg = VectorGraphics;
+
+}
